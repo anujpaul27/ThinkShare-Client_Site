407c1b6affdb2035f132c060144a8f1f16c56afc
anuj paul <paulanuj27@gmail.com>
2026-05-21 21:57:44 +0600

    fix some bug

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import Link from "next/link";
 import { Lightbulb, Mail } from "lucide-react";
 
-
 export default function Footer() {
   return (
     <footer className="bg-base-300  text-base-content border-t border-base-200">
@@ -58,7 +57,7 @@ export default function Footer() {
                 </Link>
               </li>
             </ul>
-          </div>          
+          </div>
 
           {/* Company */}
           <div>
@@ -73,7 +72,6 @@ export default function Footer() {
                 <Link href="/blog" className="hover:text-primary transition">
                   Blog
                 </Link>
-                
               </li>
               <li>
                 <Link href="/careers" className="hover:text-primary transition">
@@ -93,7 +91,6 @@ export default function Footer() {
             <h3 className="font-semibold text-lg mb-4">Get in Touch</h3>
 
             <div className="flex items-center gap-2 mb-4">
-              
               <a
                 href="mailto:hello@ideavault.com"
                 className="text-sm hover:text-primary transition"
@@ -109,27 +106,71 @@ export default function Footer() {
             {/* Social Links */}
             <div className="flex gap-4">
               <a
-                href="#"
+                href="/facebook.com"
                 className="btn btn-ghost btn-circle hover:text-blue-600"
-              >               
+              >
+                <svg
+                  class="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
               </a>
               <a
                 href="#"
                 className="btn btn-ghost btn-circle hover:text-blue-700"
               >
-               
+                <svg
+                  class="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
               </a>
               <a
                 href="#"
                 className="btn btn-ghost btn-circle hover:text-gray-800 dark:hover:text-white"
               >
-               
+                <svg
+                  class="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z"
+                    clip-rule="evenodd"
+                  />
+                  <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
+                </svg>
               </a>
               <a
                 href="#"
                 className="btn btn-ghost btn-circle hover:text-blue-600"
               >
-               
               </a>
             </div>
           </div>
