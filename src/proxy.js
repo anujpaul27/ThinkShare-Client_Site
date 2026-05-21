480d8ac3faa87e021d34a160ad3e395541384545
anuj paul <paulanuj27@gmail.com>
2026-05-21 21:07:10 +0600

    Update proxy file for private route

--- src/proxy.js
+++ src/proxy.js
@@ -1,40 +1,30 @@
 import { NextResponse } from "next/server";
-import { auth } from "./lib/auth";
 
-// This function can be marked `async` if using `await` inside
 export async function proxy(request) {
+  const { pathname } = request.nextUrl;
 
-  // define pathname for /ideas/:id check router
-  const { pathname } = request.nextUrl
-
-  // 1. call user session with headers
-  const session = await auth.api.getSession({
-    headers: request.headers,
-  });
-
-  // if need to access /ideas and not to be /ideas/:id route
+  // allow public route
   if (pathname === "/ideas") {
     return NextResponse.next();
   }
 
-  // 2. if session do not exist return to login page
-  if (!session) {
-    // get the URL who redirect to login page 
-    const currentUrl = request.nextUrl.pathname; 
+  // check cookie/session existence only (safe in Edge)
+  const sessionToken =
+    request.cookies.get("session")?.value ||
+    request.cookies.get("better-auth.session_token")?.value;
 
-    // send http://localhost:3000/login?callbackUrl=/add-idea to frontend
-    const loginUrl = new URL('/login', request.url);
-    loginUrl.searchParams.set('callbackUrl', currentUrl);
+  // if no session → redirect login
+  if (!sessionToken) {
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("callbackUrl", pathname);
 
     return NextResponse.redirect(loginUrl);
   }
 
-  // 3. if exist session return access destrinatoin route
   return NextResponse.next();
 }
 
 export const config = {
-  // 4. convert array and write route name inside of this array
   matcher: [
     "/add-idea/:path*",
     "/my-ideas/:path*",
